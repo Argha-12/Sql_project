46f745d2c70483c0150b6d9ea43da04ab720b41b
Argha-12 <165028577+Argha-12@users.noreply.github.com>
2024-05-11 17:37:52 +0530

    Update Index.sql

--- Index.sql
+++ Index.sql
@@ -1,1 +1,17 @@
 
+#show the total index_count
+
+SELECT COUNT(*) AS index_count
+FROM information_schema.statistics
+WHERE table_schema = 'rana';
+
+#show the total index_name in the database ###
+
+SELECT DISTINCT index_name
+FROM information_schema.statistics
+WHERE table_schema = 'rana';
+ 
+
+ #show the total index_name in the table ###
+ 
+SHOW INDEXES FROM employee;
