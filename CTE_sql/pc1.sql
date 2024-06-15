c475c7e69af67b1ff6a0bb0bff5cb035e8e7fd0c
Argha-12 <165028577+Argha-12@users.noreply.github.com>
2024-06-15 19:07:49 +0530

    Update pc1.sql

--- CTE_sql/pc1.sql
+++ CTE_sql/pc1.sql
@@ -1,4 +1,6 @@
 --- find emp who earn more then avaerage salary of all emp ------
+
+---with CTE ---
 select * from emp_new;
 with average_salary (avg_sal) as
 	(select avg(salary) from emp_new)
@@ -6,7 +8,7 @@ select emp_name , salary
 from emp_new E , average_salary
 where E.salary > average_salary.avg_sal;
     
-
+---with out CTE(subquery)---
 select emp_name , salary
 from emp_new 
 where salary > 	(select avg(salary) from emp_new);
