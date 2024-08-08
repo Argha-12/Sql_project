a9178cdbb8326583f39ecbee9b0847e0a99b165f
Argha-12 <165028577+Argha-12@users.noreply.github.com>
2024-08-08 12:17:00 +0530

    Update Video_Q4_SOLUTION.sql

--- 30_days_query_problem_solving/query_04/Video_Q4_SOLUTION.sql
+++ 30_days_query_problem_solving/query_04/Video_Q4_SOLUTION.sql
@@ -1,13 +1,9 @@
 -- OUTPUT 1
-select min(id) as id
-, min(name) as name
-, min(location) as location
+select min(id) as id, min(name) as name, min(location) as location
 from Q4_data;
 
 
 
 -- OUTPUT 2
-select max(id) as id
-, min(name) as name
-, min(location) as location
+select max(id) as id, min(name) as name, min(location) as location
 from Q4_data;
