882014144aa38e46e095f7c78ecd1ee72f35a8a2
Argha-12 <165028577+Argha-12@users.noreply.github.com>
2024-10-10 00:16:19 +0530

    Update P_I.sql

--- practice and interview/P_I.sql
+++ practice and interview/P_I.sql
@@ -71,4 +71,52 @@ join  Department D on e.EmployeeID = D.DepartmentID and D.DepartmentName in ('HR
 
     ####################################################################################
 
-    
+    -- Find the list of employees whose salary ranges between 2L to 3L.
+
+SELECT EmpName, Salary FROM Employee
+WHERE Salary > 200000 AND Salary < 300000
+--- OR –--
+SELECT EmpName, Salary FROM Employee
+WHERE Salary BETWEEN 200000 AND 300000
+
+####################################################################################
+
+-- Write a query to retrieve the list of employees from the same city
+SELECT E1.EmpID, E1.EmpName, E1.City
+FROM Employee E1, Employee E2
+WHERE E1.City = E2.City AND E1.EmpID != E2.EmpID
+
+#################################################################################
+-- Query to find the cumulative sum of employee’s salary.
+SELECT EmpID, Salary, SUM(Salary) OVER (ORDER BY EmpID) AS CumulativeSum
+FROM Employee
+
+##############################################################################
+-- Write a query to fetch 50% records from the Employee table.
+select * from employee
+where EmpID <= (select count(EmpID)/2 from employee)
+-----or-------
+WITH rana AS (
+    SELECT *,
+           ROW_NUMBER() OVER (ORDER BY EmpID) AS rownumber
+    FROM employee
+)
+SELECT *
+FROM rana
+WHERE rownumber <= (SELECT FLOOR(COUNT(EmpID) / 2) FROM employee);
+
+############################################################################################
+ -- Write a query to fetch even and odd rows from Employee table.
+WITH argha as (
+	select * , 
+			row_number() over (order by EmpID ) as row_num
+	from employee
+)
+select * 
+from argha
+WHERE row_num % 2 = 0
+------or------
+select *  from
+		(select * ,row_number() over (order by EmpID ) as row_num
+        from employee) as argha
+where argha.row_num % 2 = 0
